8b85c212294ff0ec1187da9f4a7c038b94bf9626
Cedricbernstein <bernstein.marketing@web.de>
2026-04-18 20:56:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -71,5 +71,5 @@ Rules for each prompt:
 });
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("SnapAI running on port " + PORT));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () =>console.log("SnapAI running on port " + PORT));
